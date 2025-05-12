33fdb5892baeee88c47ff7a61e4e5b1ffddd504d
hungzg12 <hungzg12@gmail.com>
2025-05-12 08:58:01 +0700

    Add files via upload

---
+++ maytinhthongdung index.html/trangchumaytinhjava.js
@@ -0,0 +1,21 @@
+const screen = document.getElementById('screen');
+
+function insert(value) {
+    screen.value += value;
+}
+
+function clearScreen() {
+    screen.value = '';
+}
+
+function backspace() {
+    screen.value = screen.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        screen.value = eval(screen.value);
+    } catch (error) {
+        alert('Lỗi tính toán!');
+    }
+}
